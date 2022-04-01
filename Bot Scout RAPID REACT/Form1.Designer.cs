60715c895918133be469b03ed13e69eb5c9f8558
Keaton Rozema <keatonrozema@gmail.com>
2022-04-01 12:55:04 -0400

    Made it so the form shows and stuff

--- Bot Scout RAPID REACT/Form1.Designer.cs
+++ Bot Scout RAPID REACT/Form1.Designer.cs
@@ -29,38 +29,83 @@ namespace Bot_Scout_RAPID_REACT
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.team = new System.Windows.Forms.TextBox();
+            this.match = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.teleUpper = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // button3
             // 
-            this.button1.Location = new System.Drawing.Point(454, 170);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button3.Location = new System.Drawing.Point(368, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // label4
             // 
-            this.textBox1.Location = new System.Drawing.Point(454, 287);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Team";
+            // 
+            // team
+            // 
+            this.team.Location = new System.Drawing.Point(154, 52);
+            this.team.Name = "team";
+            this.team.Size = new System.Drawing.Size(100, 20);
+            this.team.TabIndex = 2;
+            // 
+            // match
+            // 
+            this.match.Location = new System.Drawing.Point(154, 98);
+            this.match.Name = "match";
+            this.match.Size = new System.Drawing.Size(100, 20);
+            this.match.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Match";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(60, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tele Upper Cargo";
+            // 
+            // teleUpper
+            // 
+            this.teleUpper.Location = new System.Drawing.Point(157, 150);
+            this.teleUpper.Name = "teleUpper";
+            this.teleUpper.Size = new System.Drawing.Size(100, 20);
+            this.teleUpper.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(628, 539);
+            this.Controls.Add(this.teleUpper);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.match);
+            this.Controls.Add(this.team);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +115,19 @@ namespace Bot_Scout_RAPID_REACT
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox team;
+        private System.Windows.Forms.TextBox match;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox teleUpper;
     }
 }
 
--- Bot Scout RAPID REACT/Form1.cs
+++ Bot Scout RAPID REACT/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Bot_Scout_RAPID_REACT
 {
@@ -22,14 +23,182 @@ namespace Bot_Scout_RAPID_REACT
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Hello";
+            string current = Directory.GetCurrentDirectory() + "\\";
+            int autoCargo;
+
+            int teleCargo;
+
+            int climbTime;
+
+            string autoAccuracy;
+            string teleAccuracy;
+            int team1;
+            string prompt;
+
+            Console.WriteLine("Enter Scouting Data (Type q or quit to go back to menu)");
+            Console.Write("Team #: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            team.Text = Console.ReadLine();
+            if (!int.TryParse(team.Text, out team1))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("This is not a team. Going back to menu. Press enter to continue");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine();
+                menu.menu();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            if (!File.Exists(team.Text + ".txt"))
+            {
+                addTeam(team.Text);
+                addLines(team.Text);
+                
+            }
+
+
+            climbTime = (Int32.Parse(climbStart) - Int32.Parse(climbEnd));
+
+            double autoUpperInt = Double.Parse(autoUpper);
+            double autoLowerInt = Double.Parse(autoLower);
+            double autoMissedInt = Double.Parse(autoMissed);
+            autoAccuracy = (autoUpperInt + autoLowerInt) / (autoUpperInt + autoLowerInt + autoMissedInt) * 100 + "%";
+
+            double teleUpperInt = Double.Parse(teleUpper.Text);
+            double teleLowerInt = Double.Parse(teleLower);
+            double teleMissedInt = Double.Parse(teleMissed);
+            teleAccuracy = (teleUpperInt + teleLowerInt) / (teleUpperInt + teleLowerInt + teleMissedInt) * 100 + "%";
+
+            //User match data
+            writeToFile(team.Text, "Match Number: " + match.Text);
+            writeToFile(team.Text, "Taxi: " + taxi);
+            writeToFile(team.Text, "Tele Lower: " + teleLower);
+            writeToFile(team.Text, "Tele Upper: " + teleUpper);
+            int totalAuto = Int32.Parse(autoUpper) + Int32.Parse(autoLower);
+
+            if ((totalAuto).ToString().Length == 1)
+            {
+                writeToFile(team.Text.Text, "Auto Cargo Total: 0" + totalAuto);
+            }
+            else
+            {
+                writeToFile(team.Text.Text, "Auto Cargo Total: " + totalAuto);
+            }
+            writeToFile(team.Text, "Auto Upper: " + autoUpper);
+            writeToFile(team.Text, "Auto Lower: " + autoLower);
+            writeToFile(team.Text, "Auto Missed: " + autoMissed);
+
+            writeToFile(team.Text, "Tele Missed: " + teleMissed);
+
+
+            writeToFile(team.Text, "Start: " + climbStart);
+            writeToFile(team.Text, "End: " + climbEnd);
+            if ((climbTime).ToString().Length == 1)
+            {
+                writeToFile(team.Text.Text, "Total Climb Time: 0" + climbTime);
+            }
+            else
+            {
+                writeToFile(team.Text.Text, "Total Climb Time: " + climbTime);
+            }
+            writeToFile(team.Text, "Climb Type: " + climbType);
+            writeToFile(team.Text, "Defense: " + "0" + defense);
+            writeToFile(team.Text, "Position: " + position);
+            writeToFile(team.Text, "Entry: " + climbEnter);
+
+            teleCargo = (Int32.Parse(teleUpper) * 2) + (Int32.Parse(teleLower) * 1);
+            autoCargo = (Int32.Parse(autoUpper) * 4) + (Int32.Parse(autoLower) * 2);
+            //Calculated match data
+
+
+            if ((teleCargo).ToString().Length == 1)
+            {
+                writeToFile(team.Text.Text, "Tele Cargo Score: 0" + teleCargo);
+            }
+            else
+            {
+                writeToFile(team.Text.Text, "Tele Cargo Score: " + teleCargo);
+            }
+            if ((autoCargo).ToString().Length == 1)
+            {
+                writeToFile(team.Text.Text, "Auto Cargo Score: 0" + autoCargo);
+            }
+            else
+            {
+                writeToFile(team.Text.Text, "Auto Cargo Score: " + autoCargo);
+            }
+
+
+            writeToFile(team.Text.Text, "Auto Accuracy: " + autoAccuracy);
+            writeToFile(team.Text.Text, "Tele Accuracy: " + teleAccuracy);
+
+            writeToFile(team.Text.Text, "------------------------");
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("***********************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine(autoAccuracy);
+            Console.WriteLine("Success!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("***********************");
+
+            Thread.Sleep(2500);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            menu.menu();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        public string GetLine(string fileName, int line)
         {
+            string current = Directory.GetCurrentDirectory() + "//";
+            using (var sr = new StreamReader(fileName))
+            {
+                for (int i = 1; i < line; i++)
+                    sr.ReadLine();
+                return sr.ReadLine();
+            }
+        }
+
+        public void WriteLine(string fileName, int line)
+        {
+            string current = Directory.GetCurrentDirectory() + "\\";
+            using (var sr = new StreamWriter(fileName))
+            {
+                for (int i = 1; i < line; i++)
+                    sr.WriteLine();
+            }
+        }
+
+        public void lineChanger(string newText, string fileName, int line_to_edit)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            arrLine[line_to_edit - 1] = newText;
+            File.WriteAllLines(fileName, arrLine);
 
         }
+        public void writeToFile(string team, string text)
+        {
+            string current = Directory.GetCurrentDirectory() + "/";
+            int i = 1;
+            bool emptyLine = false;
+            while (emptyLine == false)
+            {
+                //Thread.Sleep(10);
+                if (GetLine(current + team + ".txt", i) != GetLine(current + "blank.txt", 1))
+                {
+                    //Console.WriteLine(GetLine(team+".txt", i));
+                }
+                else
+                {
+                    //Console.WriteLine("Wrote to line "+ i);
+                    lineChanger(text, current + team + ".txt", i);
+                    emptyLine = true;
+                }
+                i++;
+            }
+        }
+
+
     }
 }
